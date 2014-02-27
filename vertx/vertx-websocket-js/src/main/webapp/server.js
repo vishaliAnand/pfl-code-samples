bdf01eba99d84dce021e4f7a3a02e59f8fb3826d
jijisv <jijisv@gmail.com>
2014-02-27 12:49:21 +0000

    comments added renamed shared map name

--- vertx/vertx-websocket-js/src/main/webapp/server.js
+++ vertx/vertx-websocket-js/src/main/webapp/server.js
@@ -3,37 +3,49 @@ var console = require('vertx/console');
 
 var port = 80;
 
+// create an HTTP server.
 var server = vertx.http.createHttpServer(); 
 
-//http handler to serve html, js, css etc 
+// http handler to serve html, js, css etc.
 server.requestHandler(function(req) {
-	req.response.sendFile('.' + (req.path() === '/' ? '/index.html' : req.path()));
+    req.response.sendFile('.' + (req.path() === '/' ? '/index.html' : req.path()));
 });
 
-//websocket handler
+// websocket handler to server ws:// requests.
 server.websocketHandler(function(websocket) {
-	
-	if (websocket.path() != '/chat') {
-		websocket.reject();
-		return;
-	} 
-	
-	var wsSessions = vertx.sharedData.getSet('websocket.sessions');
-	console.log("Request received - " + websocket.textHandlerID());
-	wsSessions.add(websocket.textHandlerID());
-	
-	websocket.dataHandler (function(buffer){
-		console.log('Got a message - ' + buffer.toString());
-		wsSessions.toArray().forEach(function(handlerId){
-			console.log('Sending to ' + handlerId);
-			vertx.eventBus.send(handlerId, buffer.toString());
-		});
-	});
-	
-	websocket.closeHandler (function() {
-		console.log('Socket Closed. Removing ' + websocket.binaryHandlerID());
-		wsSessions.remove(websocket.textHandlerID());
-	});
+    
+    // accept the websocket connection only for the url '/chat'.
+    if (websocket.path() != '/chat') {
+        websocket.reject();
+        return;
+    } 
+    
+    console.log("Request received - " + websocket.textHandlerID());
+
+    // Add the textHandlerID of the websocket to a shared Set. The Websocket will 
+    // register itself to eventBus with textHandlerId for text messages. We will
+    // use this later when we want to send messages from one websocke to another.
+    var wsSessions = vertx.sharedData.getSet('websocket.chat.sessions');
+    wsSessions.add(websocket.textHandlerID());
+    
+    // handles the message from client over websocket.
+    websocket.dataHandler (function(buffer){
+        console.log('Got a message - ' + buffer.toString());
+
+        // iterate over the textHandlerIDs and publish the message to eventBus.
+        wsSessions.toArray().forEach(function(textHandlerID){
+            console.log('Sending to ' + textHandlerID);
+            vertx.eventBus.send(textHandlerID, buffer.toString());
+        });
+    });
+    
+    // handles websocket close event.
+    websocket.closeHandler (function() {
+        console.log('Socket Closed. Removing ' + websocket.binaryHandlerID());
+        // removes the textHandlerID from the shared set.
+        wsSessions.remove(websocket.textHandlerID());
+    });
 });
-// start
+
+// start the server
 server.listen(port);
